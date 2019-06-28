271a449b2ceaf6cf7fce226bf190becd1b04f9da
Tony  Fu <tonyfu@Tonys-MacBook-Pro.local>
2019-06-28 19:17:07 -0400

    remove if block for provider generation

---
+++ theme_redir.js
@@ -0,0 +1,26 @@
+var canvasURL = 'https://yale.instructure.com';
+var externalHomePageRedirects = {
+  10184:3673,
+  38082:3671,
+  38081:3817,
+  38080:3819,
+  38083:3820,
+//48036: 5273,
+    48036:3671
+//48080: 5542
+  //add all ids after adding apps
+};
+
+var homeRegex = new RegExp('^/courses/([0-9]+)/?$');
+if (homeRegex.test(window.location.pathname)) {
+  // On the course homepage
+  var matches = homeRegex.exec(window.location.pathname);
+  var courseId = matches[1];
+  if(courseId in externalHomePageRedirects){
+    window.location.replace(canvasURL+'/courses/'+courseId+'/external_tools/'+externalHomePageRedirects[courseId]);        
+  }
+}
+
+$(document).ready(function () {
+  $("iframe").css('height','200%');
+});
